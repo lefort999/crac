e523bc0040f2516ecd8024c1dc8d27fcc80be383
lefort999 <jlefort251@gmail.com>
2025-08-08 11:38:40 +0200

    Update prof41.py

--- prof41.py
+++ prof41.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("index.html")
+def accueil():
+    noms = ["Jacques", "Paul", "Pierre"]
+    return render_template("index.html", noms=noms)
+
+@app.route("/bonjour/<prenom>")
+def bonjour(prenom):
+    return render_template("bonjour.html", prenom=prenom)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
