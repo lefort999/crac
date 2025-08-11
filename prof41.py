b76b0bcf8b6c2288b3d28c597a7f66b8438a5696
lefort999 <jlefort251@gmail.com>
2025-08-11 14:48:04 +0200

    Update prof41.py

--- prof41.py
+++ prof41.py
@@ -15,7 +15,10 @@ def status(nom):
     return render_template("chapitre.html", image=image, nom=nom)
 
 if __name__ == "__main__":
-    app.run(port=10000, debug=True)
+    # app.run(port=10000, debug=True)
+
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
 
 
 
